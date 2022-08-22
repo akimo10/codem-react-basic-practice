1ccdb9eda8553de6612aabf40be0b9bba0a4c884
akim010 <akim123msk+github@gmail.com>
2022-08-22 12:46:30 +0900

    アロー関数勉強

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,19 @@
 function fn(number) {
   return number * 2;
 }
+
+//アロー関数
+const fnArrow = (number) => {
+  return number * 2;
+}
+console.log(fnArrow(2));
+
+//省略形(返却値が1行の場合)
+const fnArrow2 = number => number * 2;
+console.log(fnArrow2(2));
+
+//オブジェクトの返却
+const fnArrowObj = (number) => (
+  {result: number*2}
+);
+console.log(fnArrowObj(5))
