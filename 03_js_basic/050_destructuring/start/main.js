afa1eb0ae2602263307cedfd03b8f37fc6d200ae
akim010 <akim123msk+github@gmail.com>
2022-08-22 13:41:25 +0900

    分割代入を勉強する

--- 03_js_basic/050_destructuring/start/main.js
+++ 03_js_basic/050_destructuring/start/main.js
@@ -2,19 +2,41 @@ const arry = ["配列1", "配列2", "配列3"];
 console.log(arry[0]);
 console.log(arry[2]);
 
+//上記配列を分割代入する場合
+const [a,b,c] = ["配列1", "配列2", "配列3"];
+console.log('分割代入：'+a);
+console.log('分割代入：'+c);
+
 const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
 console.log(obj.x);
 console.log(obj.y);
+
+//上記オブジェクトを分割代入する場合
+const {x,z} = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
+console.log('分割代入：'+x);
+console.log('分割代入：'+z);
 
 const arr = ["Japan", "Tokyo", "Shinjuku"];
 const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
 
 const fnArr = (arry) => {
-  console.log("---配列---");
-  console.log(`country: ${arry[0]}`);
-  console.log(`state: ${arry[1]}`);
-  console.log(`city: ${arry[2]}`);
+  console.log("分割代入：---配列---");
+  console.log(`分割代入：country: ${arry[0]}`);
+  console.log(`分割代入：state: ${arry[1]}`);
+  console.log(`分割代入：city: ${arry[2]}`);
 };
+
+//上記関数を利用する分割代入
+const fnArr2 = ([country, state, city]) => {
+  console.log("---配列---");
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city}`);
+};
+
+fnArr(arr);
+fnArr2(arr);
+
 
 const fnObj = (objAddr) => {
   console.log("---オブジェクト---");
@@ -23,5 +45,12 @@ const fnObj = (objAddr) => {
   console.log(`city: ${objAddr.city}`);
 };
 
-fnArr(arr);
+//上記関数を利用する分割代入
+const fnObj2 = ({country, state, city}) => {
+  console.log("---オブジェクト---");
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city}`);
+};
+
 fnObj(objAddress);
