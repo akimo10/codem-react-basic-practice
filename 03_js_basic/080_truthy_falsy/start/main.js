e79ba101a41ce065eb455f67547f2e9e4de9a3c5
akim010 <akim123msk+github@gmail.com>
2022-08-22 14:06:54 +0900

    色々な真偽値と。論理積、論理和を勉強する。

--- 03_js_basic/080_truthy_falsy/start/main.js
+++ 03_js_basic/080_truthy_falsy/start/main.js
@@ -21,18 +21,18 @@ console.log(Boolean(truthy));
 console.log(Boolean(falsy));
 
 // 論理積 (&&) について
-const resultA = "" && "foo";
-const resultB = 2 && 1 && 0 && 3;
-const resultC = "foo" && 4;
+const resultA = "" && "foo";//falsyな値を格納する。
+const resultB = 2 && 1 && 0 && 3;//falsyな値を格納する。
+const resultC = "foo" && 4;//全てTrueの場合、最後の値を格納する。
 
 // console.log(resultA);
 // console.log(resultB);
 // console.log(resultC);
 
 // 理論和 (||) について
-const resultD = "" || "foo";
-const resultE = 0 || 2 || 0;
-const resultF = "foo" || 4;
+const resultD = "" || "foo";//truthyな値を格納する。
+const resultE = 0 || 2 || 0 ;//truthyな値を格納する。
+const resultF = "foo" || 4;//全てtruthyの場合は、最初の値を格納する。
 
 console.log(resultD);
 console.log(resultE);
