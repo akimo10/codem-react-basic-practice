456396dad21a6d77d9797f2b0303109112171360
akim010 <akim123msk+github@gmail.com>
2022-08-23 14:26:48 +0900

    props練習問題実施

--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,9 +1,12 @@
 import "./Profile.css";
 
 const Profile = (props) => {
+  const {name="John Doe", age="??", country="Japan"} = props;
   return (
     <div className="profile">
-
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <h3>From: {country}</h3>
     </div>
   );
 };
