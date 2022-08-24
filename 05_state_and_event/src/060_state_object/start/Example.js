5e13b6a3d5d0bde9da6b919a91e1369f43031e31
akim010 <akim123msk+github@gmail.com>
2022-08-24 16:23:29 +0900

    オブジェクト型のステートを管理、更新する方法を勉強する

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -2,6 +2,25 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj);
+
+  const changeName = (e) => {
+    setPerson({name: e.target.value, age: person.age});
+  }
+
+  const changeAge = (e) => {
+    setPerson({name: person.name, age: e.target.value});
+  }
+  return (
+    <>
+      <h3>{`Name:${person.name}`}</h3>
+      <h3>{`Age:${person.age}`}</h3>
+
+      <input type="text" onChange={changeName} value={person.name} />
+      <input type="text" onChange={changeAge} value={person.age} />
+
+    </>
+  );
 };
 
 export default Example;
