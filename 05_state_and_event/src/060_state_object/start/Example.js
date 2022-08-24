7c364f94bf7c1c92b7aa691ea4f0bbbcee4d0321
akim010 <akim123msk+github@gmail.com>
2022-08-24 16:27:11 +0900

    ちょっと修正

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -11,14 +11,21 @@ const Example = () => {
   const changeAge = (e) => {
     setPerson({name: person.name, age: e.target.value});
   }
+
+  const reset = () => {
+    setPerson({name: "", age: ""});
+  }
   return (
     <>
       <h3>{`Name:${person.name}`}</h3>
       <h3>{`Age:${person.age}`}</h3>
 
       <input type="text" onChange={changeName} value={person.name} />
-      <input type="text" onChange={changeAge} value={person.age} />
+      <input type="number" onChange={changeAge} value={person.age} />
 
+      <div>
+        <button onClick={reset}>リセット</button>
+      </div>
     </>
   );
 };
