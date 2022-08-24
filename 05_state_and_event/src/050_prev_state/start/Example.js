fb3b885b571b3a182cdff02e85627d4d4c20c0bc
akim010 <akim123msk+github@gmail.com>
2022-08-24 16:12:09 +0900

    ステート練習問題実施(カウントアップ)

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,26 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+    const [count, setCount] = useState(0);
+    const countUp = () => {
+        //実際にcountが変更されるのは再レンダリングの時である。
+        // setCount(count + 1);
+        setCount(prevstate => prevstate + 1);
+        console.log(count)
+    }
+
+    const countDown = () => {
+        setCount(count - 1);
+        console.log(count);
+    }
+
+    return (
+        <>
+            <p>現在のカウント数：{count}</p>
+            <button onClick={countUp}>+</button>
+            <button onClick={countDown}>-</button>
+        </>
+    );
 };
 
 export default Example;
