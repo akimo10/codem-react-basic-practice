682d97c9345bc312c5f5ab3d183b26daec9fd9b2
akim010 <akim123msk+github@gmail.com>
2022-08-25 14:42:55 +0900

    制御構文を練習する

--- 06_control_and_form/src/060_conditional_render/start/Example.js
+++ 06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,35 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            //??(null型演算子、null・undefinedの場合に右辺の値を代入する)
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          // .map((animal) => (
+          //   <li key={animal}>{animal}</li>
+          // ))}
+          .map((animal) => {
+            //三項演算子での条件分岐
+            // return <li key={animal}>{
+            //   animal + (animal === "Dog" 
+            //   ? "★"
+            //   : "")
+            //   }</li> ;
+
+            //&&条件(真偽値は画面に表示されないための書き方)
+            return <li key={animal}>{animal ?? 'null,undefinedでした'}{
+              animal === "Dog" && "★"
+            }</li> ;
+
+
+            //if文での条件分岐
+            // if(animal === 'Dog'){
+            //   return <li key={animal}>{animal}★</li>
+            // }else {
+            //   return <li key={animal}>{animal}</li>
+            // }
+          })}
       </ul>
     </>
   );
