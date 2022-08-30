86a3690367e0698084c818ab3bbb184cbeb6e7c8
akim010 <akim123msk+github@gmail.com>
2022-08-30 16:26:09 +0900

    カスタムフックの練習問題

---
+++ 12_hooks_p2/src/075_practice_customHook/start/hooks.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export const useCount = () => {
+    const [count, setCount] = useState(0);
+
+    const countUp = () => setCount(prev => prev + 1);
+
+    return {
+        count: count,
+        countUp: countUp
+    }
+}
