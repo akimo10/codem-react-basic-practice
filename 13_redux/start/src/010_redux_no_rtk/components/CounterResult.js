b44fa18fba6ab140688a999253242412bfe07ee4
akim010 <akim123msk+github@gmail.com>
2022-08-30 21:29:44 +0900

    redux toolkitでのミュータブルな値の変更について勉強する

--- 13_redux/start/src/010_redux_no_rtk/components/CounterResult.js
+++ 13_redux/start/src/010_redux_no_rtk/components/CounterResult.js
@@ -3,8 +3,8 @@ import { useCounter } from "../context/CounterContext";
 
 const CounterResult = () => {
   // const state = useCounter();
-  const state = useSelector(state => state.counter);
-  return <h3>{state}</h3>;
+  const count = useSelector(state => state.counter.count);
+  return <h3>{count}</h3>;
 };
 
 export default CounterResult;
